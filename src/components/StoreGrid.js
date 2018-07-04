20b04631ba648cd6d844a0d8d477a7f8fbd88af0
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-07-03 22:53:34 -0400

    built out store grid with placeholders

--- src/components/StoreGrid.js
+++ src/components/StoreGrid.js
@@ -2,18 +2,42 @@ import React, { Component } from 'react';
 import '../App.css';
 
 class StoreGrid extends Component {
-    render() {
-      return (
-        <div className="StoreGrid">
-          <div className="grid-container store-grid">
+  render() {
+    return (
+      <div className="StoreGrid">
+        <div className="grid-container store-grid">
           <div className="grid-item grid-item-store">
-              <p>I'm a grid!</p>
-            </div>
+            <p>sugar</p>
           </div>
-  
+          <div className="grid-item grid-item-store">
+            <p>flour</p>
+          </div>
+          <div className="grid-item grid-item-store">
+            <p>milk</p>
+          </div>
+          <div className="grid-item grid-item-store">
+            <p>eggs</p>
+          </div>
+          <div className="grid-item grid-item-store">
+            <p>seeds</p>
+          </div>
+          <div className="grid-item grid-item-store">
+            <p>seeds</p>
+          </div>
+          <div className="grid-item grid-item-store">
+            <p>seeds</p>
+          </div>
+          <div className="grid-item grid-item-store">
+            <p>seeds</p>
+          </div>
+          <div className="grid-item grid-item-store">
+            <p>seeds</p>
+          </div>
         </div>
-      );
-    }
+
+      </div>
+    );
   }
-  
-  export default StoreGrid;
+}
+
+export default StoreGrid;
