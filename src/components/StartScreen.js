459728eda25ef28252db8f98538f92c86c05a309
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-07-07 23:25:02 -0400

    style tweaks

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -5,9 +5,11 @@ class StartScreen extends Component {
   render() {
     return (
       <div className="StartScreen">
+      <div className="start-border">
       <p className='start'>Are you sure you want to start over?  You'll lose any progress you've made so far!</p>
       <button className='btn'>Yes, I'm sure!</button>
       <button className='btn'>Nevermind.</button>
+      </div>
       </div>
     );
   }
