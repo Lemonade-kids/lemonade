5e686fed7990ad8b070b9ce7e49aa0f18cc3efd0
danielle frankel <daniellefrankel@danielle-frankels-MacBook-2.local>
2018-07-17 19:30:07 -0400

    show/hide

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -15,71 +15,85 @@ class LogIn extends Component {
 
     // This binding is necessary to make `this` work in the callback
     this.handleClick = this.handleClick.bind(this);
+    this.state = {gender: "male"};
   }
 
-  handleClick() {
-    API.newUser()//json obj
+  handleClick(event) {
+    event.preventDefault(); 
+    API.newUser()
     this.setState({
-      letsPlay: <StartScreen />
+      letsPlay: <StartScreen/>, 
+      gender: "female"
     });
   }
 
   render() {
+    var FirstDiv = "";
+    var SecondDiv = "";
+    if(this.state.gender === "male"){
+        SecondDiv = "none";
+        FirstDiv = "block";
+    }
+    else{
+        FirstDiv = "none";
+        SecondDiv = "block";
+    } 
+
     return (
-      <div className="LogIn">
-        <form>
-          <div className="form-group row">
-            <label className="sr-only" htmlFor="inlineFormInputGroup">Create Username</label>
-            <label className="sr-only" htmlFor="inlineFormInputGroupUsername2"> Create Username</label>
-            <div className="input-group mb-2 mr-sm-2">
-              <div className="input-group-prepend">
+      <div gender="male" className={FirstDiv}>
+        <div className="LogIn">
+          <form>
+            <div style={{display:FirstDiv}} className="form-group row">
+              <label className="sr-only" htmlFor="inlineFormInputGroup">Create Username</label>
+              <label className="sr-only" htmlFor="inlineFormInputGroupUsername2"> Create Username</label>
+              <div className="input-group mb-2 mr-sm-2">
+                <div className="input-group-prepend">
+                </div>
+                <input type="text" className="form-control col-sm-5" id="inlineFormInputGroupUsername2" placeholder="Create Username"></input>
               </div>
-              <input type="text" className="form-control col-sm-5" id="inlineFormInputGroupUsername2" placeholder="Create Username"></input>
-            </div>
-            <div className="form-group row">
-              <label htmlFor="inputPassword" className="col-sm-2 col-form-label"></label>
-              <div className="col-sm-1"/>
-              <div className="col-sm-9 password-box">
-                <input type="password" className="form-control" id="inputPassword" placeholder=" Create Password"></input>
+              <div className="form-group row">
+                <label htmlFor="inputPassword" className="col-sm-2 col-form-label"></label>
+                <div className="col-sm-1" />
+                <div className="col-sm-9 password-box">
+                  <input type="password" className="form-control" id="inputPassword" placeholder=" Create Password"></input>
+                </div>
               </div>
             </div>
-          </div>
-          <button className="btn btn-outline-primary" onClick={this.handleClick.bind(this)}>Submit</button>
+            <div style={{display:FirstDiv}} className="form-group row">
+              <label className="sr-only" htmlFor="inlineFormInputGroup">Username</label>
+              <label className="sr-only" htmlFor="inlineFormInputGroupUsername2">Username</label>
+              <div className="input-group mb-2 mr-sm-2">
+                <div className="input-group-prepend">
+                </div>
+                <input type="text" className="form-control col-sm-5" id="inlineFormInputGroupUsername2" placeholder="Username"></input>
+              </div>
+              <div className="form-group row">
+                <label htmlFor="inputPassword" className="col-sm-2 col-form-label"></label>
+                <div className="col-sm-1" />
+                <div className="col-sm-9 password-box">
+                  <input type="password" className="form-control" id="inputPassword" placeholder="Password"></input>
+                </div>
+              </div>
+              <button className="btn btn-outline-primary" onClick={this.handleClick.bind(this)}>Submit</button>
+            </div>
+           
+          <div style={{display:SecondDiv}} gender="female" className={SecondDiv}>
+            <div className="loginChoice">
+              <img src={Lemons} alt="lemons" className="loginLemon loginFruit" />
+              <button type="button" className="btn btn-outline-secondary">Lemons</button>
 
-          <div className="form-group row">
-            <label className="sr-only" htmlFor="inlineFormInputGroup">Username</label>
-            <label className="sr-only" htmlFor="inlineFormInputGroupUsername2">Username</label>
-            <div className="input-group mb-2 mr-sm-2">
-              <div className="input-group-prepend">
-              </div>
-              <input type="text" className="form-control col-sm-5" id="inlineFormInputGroupUsername2" placeholder="Username"></input>
+              <img src={Blueberries} alt="blueberry" className="loginBlueberry loginFruit" />
+              <button type="button" className="btn btn-outline-secondary">Blueberries</button>
+
+              <img src={Squashes} alt="squash" className="loginSquash loginFruit" />
+              <button type="button" className="btn btn-outline-secondary">Squash</button>
             </div>
-            <div className="form-group row">
-              <label htmlFor="inputPassword" className="col-sm-2 col-form-label"></label>
-              <div className="col-sm-1"/>
-              <div className="col-sm-9 password-box">
-                <input type="password" className="form-control" id="inputPassword" placeholder="Password"></input>
-              </div>
+            <button className="btn btn-outline-primary" onClick={this.handleClick.bind(this)}>Let's play!</button>
             </div>
-          </div>
-          <button className="btn btn-outline-primary" onClick={this.handleClick.bind(this)}>Login!</button>
+            </form>
           
-          <div className="loginChoice">
-            <img src={Lemons} alt="lemons" className="loginLemon loginFruit" />
-            <button type="button" className="btn btn-outline-secondary">Lemons</button>
-
-            <img src={Blueberries} alt="blueberry" className="loginBlueberry loginFruit" />
-            <button type="button" className="btn btn-outline-secondary">Blueberries</button>
-
-            <img src={Squashes} alt="squash" className="loginSquash loginFruit" />
-            <button type="button" className="btn btn-outline-secondary">Squash</button>
-
-          </div>
-
-        </form>
-        <button className="btn btn-outline-primary" onClick={this.handleClick.bind(this)}>Let's play!</button>
+        </div>
       </div>
-
     );
   }
 }
