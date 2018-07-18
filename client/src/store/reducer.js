59a62fa01179687c13f09e50196d2c66d9b179c3
Nate Dent <mixedguy@Nates-MacBook-Pro.local>
2018-07-17 20:55:45 -0400

    prayer

---
+++ client/src/store/reducer.js
@@ -0,0 +1,36 @@
+const initialState = {
+    login: "",
+    password: "",
+    Bank: 123,
+    Lemons: 12,
+    squash: 10,
+    blueberries: 0,
+    product: 123,
+    gardenGrowth: 0,
+    flour: 10,
+    sugar: 100,
+    productPrice: 0,
+    winnings: 0 ,
+    water: false,
+    harvested:false,
+    cooked: false,
+    weeded: false
+
+}
+
+
+const reducer = (state = initialState, action) => {
+    if (action.type === 'SAVE') {
+         return Object.assign({}, state, { Bank: state.Bank + action.Bank })
+         return Object.assign({}, state, { product: state.product - action.inventory })
+    } else if(action.type === "GAME") {
+        console.log(state.winnings + state.Bank)
+    } else if(action.type === 'SAVEPROD'){
+        return Object.assign({}, state, { product: state.product - action.inventory })
+    }
+    return state;
+    
+
+};
+
+export default reducer;
